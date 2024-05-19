b8712d8c64f4738f6ca71873674f89ad1dc0c1c4
Eillmaa <nazninjamshedali@gmail.com>
2024-05-19 16:04:13 +0400

    navbar for characters

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-window.onscroll = function () {
-  scrollingFunction();
-};
-function scrollingFunction() {
-  if (document.body.scrollTop > 35 || document.documentElement.scrollTop > 35) {
-    document.getElementById("navibar").style.padding = "20px 10px";
-  } else {
-    document.getElementById("navibar").style.padding = "60px 10px";
-  }
-}
+
