70aefabbaba716365350dfa11b224f5277b4b1f0
Eillmaa <nazninjamshedali@gmail.com>
2024-05-22 21:05:39 +0400

    enemies

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-slider").mount();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-slider", {
+    cover: true,
+    heightRatio: 0.5,
+  }).mount();
+});
