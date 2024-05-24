52d5a8f349eaebe41c0604ced2a25527802e3eef
Eillmaa <nazninjamshedali@gmail.com>
2024-05-24 21:59:21 +0400

    image slider for enemies

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#image-slider").mount();
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+// Here I made it so that you can scroll horizontally when hovering over the images
+let isDragging = false, startX, startScrollLeft;
+
+// Here it gets the number of cards that can fit in the carousel at once
+let cardPerview = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerview).reverse().forEach(card => {
+  carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
-document.addEventListener("DOMContentLoaded", function () {
-  new Splide("#image-slider", {
-    cover: true,
-    heightRatio: 0.5,
-  }).mount();
+
+carouselChildrens.slice(0, cardPerview).forEach(card => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
+
+//  Here I added an event listener for the arrows to scroll the carousel left or right
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  if(carousel.scrollLeft === 0) {
+    console.log("You've reached the left end");
+  } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    console.log("You've reached the right end");
+  }
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
